7f694ca86a2cb7ecf1cff6c9e5832c2703185d94
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-08 23:13:40 +0530

    Time: 1 ms (34.02%) | Memory: 28.3 MB (7.57%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        priority_queue<pair<int,int>>p;
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+        }
+        for(auto& m:mp){
+            p.push({m.second,m.first});
+        }
+        return p.top().second;
+    }
+};
