f10efb72d4738ab26ed5f6850780c70b3698af43
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-06-21 10:54:28 +0530

    Time: 29 ms (68.90%) | Memory: 83.7 MB (28.30%) - LeetSync

--- 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -1,30 +1,41 @@
-class Solution {  //by sorting and not optimal
+// class Solution {  //by sorting and not optimal
+// public:
+//     int maxIceCream(vector<int>& costs, int coins) {
+//         int max_icreams=0;
+//         sort(costs.begin(),costs.end());
+//         int index=0;
+//         for(int i=0;i<costs.size();i++){
+//             if(coins<costs[i]){
+//                 break;
+//             }
+//             else{
+//                 coins-=costs[i];
+//                 max_icreams++;
+//             }
+//         }
+//         return max_icreams;
+//     }
+// };
+
+
+class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
         int max_icreams=0;
-        sort(costs.begin(),costs.end());
-        int index=0;
+        priority_queue<int,vector<int>,greater<int>> pq(costs.begin(),costs.end());
         for(int i=0;i<costs.size();i++){
-            if(coins<costs[i]){
+            if(coins<pq.top()){
                 break;
             }
             else{
-                coins-=costs[i];
+                coins-=pq.top();
+                pq.pop();
                 max_icreams++;
             }
         }
         return max_icreams;
     }
 };
-
-
-// class Solution {
-// public:
-//     int maxIceCream(vector<int>& costs, int coins) {
-//         int max_icreams=0;
-        
-//     }
-// };
 
 
 
