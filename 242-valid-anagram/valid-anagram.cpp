73596aa127a09dd64f33ea4caadee7b99df0cb82
snehalgarg05-cyber <snehalgarg05@gmail.com>
2026-07-17 20:05:28 +0530

    Time: 3 ms (45.97%) | Memory: 9.8 MB (25.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.size() != t.size())
+        if(s.length()!=t.length()){
             return false;
-
-        unordered_map<char,int> mp;
-
-        for(char ch : s)
-            mp[ch]++;
-
-        for(char ch : t)
-            mp[ch]--;
-
-        for(auto m : mp){
-            if(m.second != 0)
-                return false;
         }
-
-        return true;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            freq1[s[i]-'a']++;
+            freq2[t[i]-'a']++;
+        }
+        if(freq1==freq2){
+            return true;
+        }
+        return false;
     }
 };
